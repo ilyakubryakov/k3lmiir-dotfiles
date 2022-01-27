f96fb833a59d2dc7a14ec8db588e4a4d89b25c7c
Ilya Kubryakov <k3lmiir@gmail.com>
2022-01-28 00:08:37 +0200

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,40 @@
+# Script for the quick initial config macOS to strart working with Ya.Praktikum
+## Usage
+Open Terminal program (Open Spootlight and start typing Terminal or `Applicatin -> Utilities -> Terminal` ) and paste following code:
+```bash
+curl бла бла
+```
+The script will install the following application:
+
+```
+Xcode Command Line Tools
+Homebrew
+Oh-my-zsh
+iTerm2
+python@3.9
+virtualenv
+wget
+curl
+git
+zsh
+```
+and following python libs:
+```
+attrs==21.2.0
+flake8==4.0.1
+importlib-metadata==4.8.1
+iniconfig==1.1.1
+mccabe==0.6.1
+packaging==21.0
+pluggy==1.0.0
+py==1.10.0
+pycodestyle==2.8.0
+pyflakes==2.4.0
+pyparsing==2.4.7
+pytest==6.2.5
+pytest-tldr==0.2.4
+toml==0.10.2
+typing-extensions==3.10.0.2
+zipp==3.6.0
+```
+Feel free to add something via pull request, I wrote this in half an hour to help studens easy configure their first environment and start pythoning :)
---
+++ config.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#Check Xcode-select installed and install Homebrew
+check_xcode = `xcode-select -p 1>/dev/null;echo $?`
+if [[check_xcode == 0]]; then
+    #Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    xcode-select --install
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+#Install brew's
+for i in $(cat brew.txt); do
+    brew install "$i" 
+done
+
+#Install brew casks
+for i in $(cat brew_cask.txt); do 
+    brew install --cask "$i" 
+done
+
+#Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#Install python requirements
+pip3 install -r requirements_python.txt
+
+#Install VSCode extentions 
+code="/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code"
+cat vscode_extensions_list.txt | xargs -L 1 $code --install-extension
+
+osascript -e 'display dialog "All done! You cool" buttons {"Yeah"}'
