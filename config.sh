4d9230bf51997049ae01c51ecd9131bbe3835565
Ilya Kubryakov <k3lmiir@gmail.com>
2022-01-28 00:25:36 +0200

    Fixed an errors

--- config.sh
+++ config.sh
@@ -2,26 +2,31 @@
 #Check Xcode-select installed and install Homebrew
 xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 #Install brew's
 /bin/bash -c "$(curl  https://raw.githubusercontent.com/ilyakubryakov/macos_ya.p_initial_config/main/brew.txt --output brew.txt)"
 for i in $(cat brew.txt); do
     brew install "$i" 
 done
 rm -rf brew.txt
+
 #Install brew casks
 /bin/bash -c "$(curl  https://raw.githubusercontent.com/ilyakubryakov/macos_ya.p_initial_config/main/brew_cask.txt --output brew_cask.txt)"
 for i in $(cat brew_cask.txt); do 
     brew install --cask "$i" 
 done
 rm -rf brew_cask.txt
+
 #Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #Install python requirements
+/bin/bash -c "$(curl https://raw.githubusercontent.com/ilyakubryakov/macos_ya.p_initial_config/main/requirements_python.txt --output requirements_python.txt)"
 pip3 install -r requirements_python.txt
+rm -rf requirements_python.txt
 
 #Install VSCode extentions 
 code="/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code"
 cat vscode_extensions_list.txt | xargs -L 1 $code --install-extension
 
-osascript -e 'display dialog "All done! You cool" buttons {"Yeah"}'
+osascript -e 'display dialog "All done! You cool! 🍺" buttons {"Yeah"}'
