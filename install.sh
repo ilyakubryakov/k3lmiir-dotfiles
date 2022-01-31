c0db8e1c35743fa74bd060bb7598fafa51466e0c
Ilya Kubryakov <k3lmiir@gmail.com>
2022-01-31 10:07:16 +0200

    Created new structure

--- install.sh
+++ install.sh
@@ -1,34 +1,123 @@
 #!/bin/bash
-#Check Xcode-select installed and install Homebrew
-xcode-select --install
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+set -e
+MACOS_SCRIPT="macos/config_macos.sh"
+LINUX_SCRIPT="linux/config_linus.sh"
 
-#Install brew's
-/bin/bash -c "$(curl  https://raw.githubusercontent.com/ilyakubryakov/macos_ya.p_initial_config/main/brew.txt --output brew.txt)"
-for i in $(cat brew.txt); do
-    brew install "$i" 
-done
-rm -rf brew.txt
 
-#Install brew casks
-/bin/bash -c "$(curl  https://raw.githubusercontent.com/ilyakubryakov/macos_ya.p_initial_config/main/brew_cask.txt --output brew_cask.txt)"
-for i in $(cat brew_cask.txt); do 
-    brew install --cask "$i" 
-done
-rm -rf brew_cask.txt
+USER=${USER:-$(id -u -n)}
 
-#Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ -t 1 ]; then
+  is_tty() {
+    true
+  }
+else
+  is_tty() {
+    false
+  }
+fi
 
-#Install python requirements
-/bin/bash -c "$(curl https://raw.githubusercontent.com/ilyakubryakov/macos_ya.p_initial_config/main/requirements_python.txt --output requirements_python.txt)"
-pip3 install -r requirements_python.txt
-rm -rf requirements_python.txt
+# Source: https://gist.github.com/XVilka/8346728
+supports_truecolor() {
+  case "$COLORTERM" in
+  truecolor|24bit) return 0 ;;
+  esac
 
-#Install VSCode extentions 
-/bin/bash -c "$(curl https://raw.githubusercontent.com/ilyakubryakov/macos_ya.p_initial_config/main/vscode_extensions_list.txt --output vscode_extensions_list.txt)"
-for i in $(cat vscode_extensions_list.txt); do 
-    /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code --install-extension "$i" 
-done
-rm -rf vscode_extensions_list.txt
-osascript -e 'display dialog "All done! You cool! 🍺" buttons {"Yeah"}'
+  case "$TERM" in
+  iterm           |\
+  tmux-truecolor  |\
+  linux-truecolor |\
+  xterm-truecolor |\
+  screen-truecolor) return 0 ;;
+  esac
+
+  return 1
+}
+
+fmt_link() {
+  # $1: text, $2: url, $3: fallback mode
+  if supports_hyperlinks; then
+    printf '\033]8;;%s\a%s\033]8;;\a\n' "$2" "$1"
+    return
+  fi    
+
+  case "$3" in
+  --text) printf '%s\n' "$1" ;;
+  --url|*) fmt_underline "$2" ;;
+  esac
+}
+
+fmt_underline() {
+  is_tty && printf '\033[4m%s\033[24m\n' "$*" || printf '%s\n' "$*"
+}
+
+# shellcheck disable=SC2016 # backtick in single-quote
+fmt_code() {
+  is_tty && printf '`\033[2m%s\033[22m`\n' "$*" || printf '`%s`\n' "$*"
+}
+
+fmt_error() {
+  printf '%sError: %s%s\n' "${FMT_BOLD}${FMT_RED}" "$*" "$FMT_RESET" >&2
+}
+
+setup_color() {
+  # Only use colors if connected to a terminal
+  if ! is_tty; then
+    FMT_RAINBOW=""
+    FMT_RED=""
+    FMT_GREEN=""
+    FMT_YELLOW=""
+    FMT_BLUE=""
+    FMT_BOLD=""
+    FMT_RESET=""
+    return
+  fi
+
+  if supports_truecolor; then
+    FMT_RAINBOW="
+      $(printf '\033[38;2;255;0;0m')
+      $(printf '\033[38;2;255;97;0m')
+      $(printf '\033[38;2;247;255;0m')
+      $(printf '\033[38;2;0;255;30m')
+      $(printf '\033[38;2;77;0;255m')
+      $(printf '\033[38;2;168;0;255m')
+      $(printf '\033[38;2;245;0;172m')
+    "
+  else
+    FMT_RAINBOW="
+      $(printf '\033[38;5;196m')
+      $(printf '\033[38;5;202m')
+      $(printf '\033[38;5;226m')
+      $(printf '\033[38;5;082m')
+      $(printf '\033[38;5;021m')
+      $(printf '\033[38;5;093m')
+      $(printf '\033[38;5;163m')
+    "
+  fi
+
+  FMT_RED=$(printf '\033[31m')
+  FMT_GREEN=$(printf '\033[32m')
+  FMT_YELLOW=$(printf '\033[33m')
+  FMT_BLUE=$(printf '\033[34m')
+  FMT_BOLD=$(printf '\033[1m')
+  FMT_RESET=$(printf '\033[0m')
+}
+
+
+  ostype=$(uname)
+  if [ -z "${ostype%CYGWIN*}" ] && git --version | grep -q msysgit; then
+    fmt_error "Windows/MSYS Git is not supported on Cygwin"
+    exit 1
+  fi
+setup_color
+printf '\n'
+printf "$FMT_GREEN $FMT_BOLD %s %s %s This script will install and configure some softwere that I(k3lmiir) like to use.\n $FMT_RESET" 
+printf "$FMT_GREEN $FMT_BOLD %s %s %s Hope it will save some time for you. Enjoy :)\n $FMT_RESET"
+printf "$FMT_GREEN $FMT_BOLD %s %s %s  • Follow https://www.instagram.com/k3lmiir/" 
+printf '\n'
+
+if [ -z "${ostype%Darwin*}" ]; then
+    printf '\n'
+    printf "$FMT_YELLOW $FMT_BOLD %s %s %s ...It seems you using macOS. Well, let's configure it...\n $FMT_RESET"
+    printf '\n'
+    /bin/bash $MACOS_SCRIPT
+fi
